2514c542b54e9acaad8b5dc4de55622548aa6a40
y4shiro <y4shironao@gmail.com>
2021-12-06 21:56:47 +0900

    問13

---
+++ src/11~20/13.ts
@@ -0,0 +1,11 @@
+const q13 = () => {
+  let createObj = <T extends unknown>(obj: T) => {
+    let o = {} as { [P in keyof T]: string };
+    for (const key in obj) {
+      o[key] = String(obj[key]);
+    }
+    return o;
+  };
+
+  const anotherFun = createObj;
+};
