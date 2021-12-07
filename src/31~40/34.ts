3baaba964bade52a291041245004665bb4a8e3c2
y4shiro <y4shironao@gmail.com>
2021-12-07 18:19:18 +0900

    問34

---
+++ src/31~40/34.ts
@@ -0,0 +1,12 @@
+const q34 = () => {
+  export default function person({ detail = {} as Detail }: Person) {
+    return <div>{detail.name}</div>;
+  }
+
+  interface Person {
+    id: number;
+    detail: Detail;
+  }
+
+  person({ detail: { name: 'fafa' }, id: 1 });
+};
