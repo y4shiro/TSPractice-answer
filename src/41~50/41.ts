da83ec610ba4c17f0a59f15e2456dbaac95ba9d2
y4shiro <y4shironao@gmail.com>
2021-12-07 19:09:37 +0900

    問41

---
+++ src/41~50/41.ts
@@ -0,0 +1,6 @@
+// declare function fn(x: HTMLDivElement): string;
+// declare function fn(x: HTMLElement): number;
+// declare function fn(x: any): any;
+
+// var myElem: HTMLDivElement;
+// var x = fn(myElem); // x: any, wat?
