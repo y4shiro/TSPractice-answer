3db1d750cfedec3838d1a55809ceaa6b43bca41e
y4shiro <y4shironao@gmail.com>
2021-12-07 18:22:47 +0900

    問35

--- src/31~40/34.ts
+++ src/31~40/34.ts
@@ -1,12 +1,10 @@
 const q34 = () => {
-  export default function person({ detail = {} as Detail }: Person) {
-    return <div>{detail.name}</div>;
-  }
-
-  interface Person {
-    id: number;
-    detail: Detail;
-  }
-
-  person({ detail: { name: 'fafa' }, id: 1 });
+  // export default function person({ detail = {} as Detail }: Person) {
+  //   return <div>{detail.name}</div>;
+  // }
+  // interface Person {
+  //   id: number;
+  //   detail: Detail;
+  // }
+  // person({ detail: { name: 'fafa' }, id: 1 });
 };
---
+++ src/31~40/35.ts
@@ -0,0 +1,8 @@
+const q35 = () => {
+  // interface State {
+  //   name?: string;
+  //   age?: number;
+  // }
+  // this.setState({ name: 'kenji' }); // Error
+  // this.setState({ name: 'kenji', age: this.state.age }); // ok
+};
