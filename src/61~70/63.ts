58bc33b13f1ad19f4d68542eddd62ad6bbff463c
y4shiro <y4shironao@gmail.com>
2021-12-10 10:27:30 +0900

    問63

---
+++ src/61~70/63.ts
@@ -0,0 +1,21 @@
+const q63 = () => {
+  // function stringLiterals<T extends string>(...args: T[]): T[] {
+  //   return args;
+  // }
+  // type OneOf<T extends unknown[]> = T extends (infer R)[] ? R : never;
+
+  // const values = stringLiterals('A', 'B');
+  // type Foo = OneOf<typeof values>;
+
+  type OneOf<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<
+    infer ElementType
+  >
+    ? ElementType
+    : never;
+
+  const values = ['A', 'B'] as const;
+  type Foo = OneOf<typeof values>;
+
+  const v1: Foo = 'A'; // ok
+  const v2: Foo = 'D'; // error
+};
