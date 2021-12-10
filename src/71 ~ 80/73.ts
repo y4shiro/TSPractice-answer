e921a7c0dfad9f19582d97be40b59fc5c7f696df
y4shiro <y4shironao@gmail.com>
2021-12-10 16:39:49 +0900

    問73

---
+++ src/71 ~ 80/73.ts
@@ -0,0 +1,15 @@
+const q73 = () => {
+  type TransitionStyles<T extends string> = {
+    [K in T]: Object;
+  };
+
+  type TransitionStylesKeys = 'entered' | 'exiting' | 'exited' | 'entering';
+
+  const b: TransitionStyles<TransitionStylesKeys> = {
+    entered: {},
+    exiting: {},
+    exited: {},
+    entering: {},
+    eee: 'fa',
+  };
+};
